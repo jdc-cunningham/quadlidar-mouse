a3e7f99643d54889c165824e0959b91278cd9468
Jacob Cunningham <jcun4128@gmail.com>
2024-07-10 13:42:30 -0500

    start printing, test python mouse

---
+++ DEVLOG.md
@@ -0,0 +1,6 @@
+### 07/10/2024
+
+12:34 PM
+
+Decided to move this to its own repo
+
---
+++ software/test-code/mouse.py
@@ -0,0 +1,8 @@
+# run mouse directly with python
+# https://stackoverflow.com/a/27046948/2710227
+
+import pyautogui
+
+pyautogui.moveTo(100, 150)
+
+# yeah this works
